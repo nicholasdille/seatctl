090092047748474632f1026d38cb2be94b584a2f
Nicholas Dille <nicholas.dille@mailbox.org>
2022-05-13 23:35:08 +0200

    Added auth var for traefik

--- command/user.sh
+++ command/user.sh
@@ -78,6 +78,16 @@ function user_main() {
             docker-group)
                 run_on_seat "${name}" "${index}" usermod -aG docker "seat"
             ;;
+            var)
+                local ip
+                # shellcheck disable=SC2154
+                ip=$(jq --raw-output '.ip' "${script_base_dir}/set/${name}/seat-${name}-${index}.json")
+                local password
+                password=$(grep "^seat${index}\." "${script_base_dir}/set/${name}/passwords.csv" | cut -d';' -f3)
+                local var_value
+                var_value="$(htpasswd -nbB seat "${password}" | sed -e 's/\$/\\\$/g')"
+                echo "export SEAT_HTPASSWD=${var_value}" | ssh -i "${script_base_dir}/set/${name}/ssh" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR "root@${ip}" "cat >/etc/profile.d/seat_htpasswd.sh"
+            ;;
         esac
     done
 
