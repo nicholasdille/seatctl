ed22e1ed0a291bab847dbc49479d1678296aa2be
Nicholas Dille <nicholas.dille@mailbox.org>
2022-05-15 17:11:25 +0200

    Additional variable for portainer

--- command/user.sh
+++ command/user.sh
@@ -87,6 +87,8 @@ function user_main() {
                 local var_value
                 var_value="$(htpasswd -nbB seat "${password}" | sed -e 's/\$/\\\$/g')"
                 echo "export SEAT_HTPASSWD=${var_value}" | ssh -i "${script_base_dir}/set/${name}/ssh" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR "root@${ip}" "cat >/etc/profile.d/seat_htpasswd.sh"
+                var_value="$(htpasswd -nbB seat "${password}" | sed -e 's/\$/\\\$/g' | cut -d: -f2)"
+                echo "export SEAT_HTPASSWD_ONLY=${var_value}" | ssh -i "${script_base_dir}/set/${name}/ssh" -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR "root@${ip}" "cat >/etc/profile.d/seat_htpasswd_only.sh"
             ;;
         esac
     done
