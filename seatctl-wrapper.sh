1c89b7d1509868309540a92ff454d3585b6bcc4c
Nicholas Dille <nicholas.dille@mailbox.org>
2022-09-21 09:25:06 +0200

    Added proper waiting and updates

--- seatctl-wrapper.sh
+++ seatctl-wrapper.sh
@@ -5,7 +5,11 @@ ZONE="inmylab.de"
 
 ./seatctl.sh "$@" add
 ./seatctl.sh "$@" wait
-# TODO: Wait for cloud-init to finish
 ./seatctl.sh "$@" tls --zone "${ZONE}" --command get --sleep 30
 ./seatctl.sh "$@" tls --zone "${ZONE}" --command copy
+./seatctl.sh "$@" wait
+./seatctl.sh "$@" run -- cloud-init status --wait
+./seatctl.sh "$@" wait
+./seatctl.sh "$@" run -- docker-setup update
+./seatctl.sh "$@" run -- docker-setup upgrade
 ./seatctl.sh "$@" run -- docker-setup install / docker
